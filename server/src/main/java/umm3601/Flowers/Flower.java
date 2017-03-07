fc543c9bd152e120abdfb4eb2e4c489d15850e9f
GLOBBIMUS <striker1342566@gmail.com>
2017-03-07 15:03:50 -0600

    Add Flower Controller to implement flower functionality
    Get flowers and get beds from database.
    issue #8

---
+++ server/src/main/java/umm3601/Flowers/Flower.java
@@ -0,0 +1,13 @@
+package umm3601.Flowers;
+
+
+public class Flower{
+    String type;
+    String number;
+    String wall;
+    String desription;
+    String commonName;
+    String source;
+    String cultivar;
+    String location;
+}
